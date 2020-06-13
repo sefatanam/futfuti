756c854bc00b96fa4e6940497193fd76e1bc7adf
sefatanam <sefatanam@gmail.com>
2020-06-14 02:44:39 +0600

    Updates

---
+++ precache-manifest.ef489c9afcc78c74e5d430b252d9c6f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb02bf4615489cc68e1491672e1ab92",
+    "url": "/futfuti/index.html"
+  },
+  {
+    "revision": "f2f3430f77adc9bf7c3f",
+    "url": "/futfuti/static/css/main.7240acd4.chunk.css"
+  },
+  {
+    "revision": "c5c9ab1c5cb634aa611e",
+    "url": "/futfuti/static/js/2.a408198a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/futfuti/static/js/2.a408198a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f3430f77adc9bf7c3f",
+    "url": "/futfuti/static/js/main.6e109c5b.chunk.js"
+  },
+  {
+    "revision": "2420f34df08d55a47e88",
+    "url": "/futfuti/static/js/runtime-main.109b28f7.js"
+  }
+]);
